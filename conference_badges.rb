9414c15db25cf3bbff1118b7b1d97cc4042c4e31
modern-class-9458 <alexander.clamor@gmail.com>
2020-08-20 19:52:55 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,9 +14,9 @@ def assign_rooms(array)
 end
 
 def printer(array)
-  batch_badge_creator(array)
-  assign_rooms(array)
+  puts batch_badge_creator(array)
+  puts assign_rooms(array)
 end
 
 array = ["A","B"]
-puts printer(array)
+printer(array)
