440645116b91ff7442b9d05e7c6aa2de6ea61d59
modern-class-9458 <alexander.clamor@gmail.com>
2020-08-20 19:26:01 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,10 +4,10 @@ def batch_badge_creator(name)
 end
 
 def assign_rooms(array)
-  array.each_with_index {|name, i|
+  array.each_with_index.map {|name, i|
     "Hello, #{name}! You'll be assigned to room #{i+1}!"
   }
 end
 
-names = "Awer"
-puts batch_badge_creator(names)
+names = ["A","B"]
+puts assign_rooms(names)
